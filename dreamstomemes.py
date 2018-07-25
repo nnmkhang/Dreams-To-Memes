1b9915aa6c7b2e467d7c03553e22f9aae6e4774f
Khang Nguyen <nnmkhang166@gmail.com>
2018-07-25 18:21:43 -0400

    fixed the for loop

--- dreamstomemes.py
+++ dreamstomemes.py
@@ -1,6 +1,7 @@
 import os.path
 import praw
 import urllib.request
+import random
 print("\n")
 
 #username: memestodreams1   
@@ -26,6 +27,11 @@ def downloadURL(url): # function downloads the url specified to a folder
         #print picUrl 
         urllib.urlretrieve(picUrl, picName)
 
+
+# gets a random number between 1 and 100 
+rand = random.randint(1,100)
+
+
 print("test")
 directory = input("Where to download?")
 print(directory)
@@ -39,16 +45,16 @@ reddit = praw.Reddit(client_id = "xbN4kVxAil3Sng",
                      password = "123456",
                      user_agent = "memestodreamsscript" )
 
-sub = reddit.subreddit('greentext').top(time_filter= "all", limit =None)[10] 
- # no idea why it wont work without using the for itteration
-for x in sub:
-    print(x.title)
-    print(x.url)
-    filename = (directory+x.title.replace(" ","_")+".jpg")
-    print(filename)
-    urllib.request.urlretrieve(x.url,filename)
+sub = list(reddit.subreddit('greentext').top(time_filter= "all", limit =100))[rand] 
+ 
+
+print(sub.title)
+print(sub.url)
+filename = (directory+sub.title.replace(" ","_")+".jpg")
+print(filename)
+urllib.request.urlretrieve(sub.url,filename)
 
     #https://www.reddit.com/r/redditdev/comments/5k9zui/how_to_get_nth_most_upvoted_post_in_a_subreddit/?st=jk0fj5vi&sh=775b4848
-    #got downloading working, now gotta get random images, use rand num gen, and
+    #got downloading working, now gotta get random images, use rand num gen, and 
     # then just get that post num.
     # after use the OCR To transfer to string and write it to a file 
