0131794f7477b5ce03284b5f61ec4f95d5a4a1f2
khang <nnmkhang166@gmail.com>
2018-08-02 19:36:57 -0400

    Test

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+@app.route('/cakes')
+def cakes():
+    return 'yummy cakes'
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host='0.0.0.0')
+    
+# to access the webserver, type : https://127.0.0.1:5000/
+#127.0.0.1 means home, ( this computer ) and :5000 means port 5000 which is the port
+# that the websever is running on.
+
+#@app.route('/") determines the entery point the / means the root of the website, so
+#homepage
+#def index() is the name we give to the route/ it was called index bc its the index
+# of the website.
+
